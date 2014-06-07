434d8ec5e3792a8b4587897a2dc3da2037330f59
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-06-07 14:42:10 -0700

    added ability to send text messages and return errors via twilio module

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140607191132) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clients", force: true do |t|
     t.string   "name"
     t.integer  "phone"
---
+++ lib/twilio.rb
@@ -0,0 +1,24 @@
+module Twilio
+  extend self
+  ACCT_SID = ENV['TWILIO_ACCT_SID']
+  AUTH_TOKEN = ENV['TWILIO_AUTH_TOKEN']
+  FROM_NUM = ENV['TWILIO_NUMBER']
+
+  def send_message(number, message)
+    begin
+      client = Twilio::REST::Client.new(ACCT_SID, AUTH_TOKEN)
+      message_details = {
+        to: number,
+        from: FROM_NUM,
+        body: message
+      }
+      message = client.account.messages.create(message_details)
+    rescue Twilio::REST::RequestError => e
+      self.handle_error(e.message)
+    end
+  end
+
+  def handle_error(error_message)
+    p error_message # Determine where to send/display error message
+  end
+end
