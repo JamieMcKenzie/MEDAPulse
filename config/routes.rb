17c1220c627580b5d9c605b2d00a9c7f1aac31d1
Brittany Martin <brittany.jill.martin@gmail.com>
2014-06-07 13:59:50 -0700

    Created index and about views and began devise implementation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  root :to => 'welcome#index'
+
   resources :clients
 
   # The priority is based upon order of creation: first created -> highest priority.
