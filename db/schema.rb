4fd949747022a29fcf30c00c40abc1aaacb9d381
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-06-07 19:09:12 -0700

    associated users and clients in the database and updated seed file to clear database and associate a user with clients upon seeding

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140607232043) do
+ActiveRecord::Schema.define(version: 20140608020158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20140607232043) do
     t.string   "salesforce_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "goals", force: true do |t|
