5c982e05eead5ff65bf882ce4c14f20a5e41528e
Jamie McKenzie <jamiemckenzie415@gmail.com>
2014-06-07 19:50:20 -0700

    Modifies devise route to change controller -> registrations.

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 
   root :to => 'welcome#index'
 
-  devise_for :users, :path => 'accounts'
+  devise_for :users, :controllers => { :registrations => "registrations" }
 
   resources :users do
     resources :clients, shallow: true do
@@ -15,9 +15,6 @@ Rails.application.routes.draw do
       end
     end
   end
-
-
-
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
