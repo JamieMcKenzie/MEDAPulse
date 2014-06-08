29097768a85841e141ffab7d4c20a93f2122afeb
Jamie McKenzie <jamiemckenzie415@gmail.com>
2014-06-08 12:25:22 -0700

    Modifies seed file to have real life example.

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,19 +24,21 @@ User.destroy_all
 Client.destroy_all
 Goal.destroy_all
 
+stephen = User.create({ name: "Stephen Watkins", email: "stephen@meda.org", password: "mission94110", password_confirmation: "mission94110" })
 
-users = []
+users = [stephen]
 10.times do
   users << User.create({name: CoolFaker::Character.name, email: Faker::Internet.email, password: 'helloworld', password_confirmation: 'helloworld'})
 end
 
 clients = []
-
 users.each do |user|
   15.times do
     clients << Client.create({name: CoolFaker::Character.name, phone: Faker::Number.number(10), salesforce_id: getID, user: user})
   end
 end
+joe = Client.create({ name: "Joe Ortega", phone: "1234567890", salesforce_id: 1234567, user: stephen })
+clients << joe
 
 goals = []
 clients.each do |client|
@@ -44,6 +46,8 @@ clients.each do |client|
     goals << Goal.create({content: CoolFaker::Team.slogan, category: getCategory, num_value: getNum, client: client})
   end
 end
+college = Goal.create({ content: "Save $2000 for college.", category: "Savings", client: joe })
+goals << college
 
 steps = []
 goals.each do |goal|
@@ -51,6 +55,10 @@ goals.each do |goal|
     steps << Step.create({content: CoolFaker::Team.slogan, status: getStatus, by_when: getDate, goal: goal})
   end
 end
+step_one = Step.create({ content: "Open a bank account.", status: "active", by_when: Date.today, goal: college })
+step_two = Step.create({ content: "Save $500 in July.", status: "queued", by_when: Date.today+(40), goal: college })
+steps << step_one
+steps << step_two
 
 steps.each do |step|
   2.times do
