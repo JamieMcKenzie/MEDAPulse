d533bde4225281baeccc54b3e0e8dcc0c6e796f9
Chris Yancey <Chris@Chriss-MacBook-Pro-2.local>
2014-06-07 19:58:47 -0700

    when viewing individual client, goals are listed as links which direct toward the individual goal show route

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -10,7 +10,7 @@ class ClientsController < ApplicationController
   # GET /clients/1
   # GET /clients/1.json
   def show
-    # @client = Client.find(params[:id])
+    @goals = @client.goals
   end
 
   # GET /clients/new
